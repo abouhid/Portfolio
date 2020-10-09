57b23b5e5a8dd5c8ae6ea8f9b994c1d96387c654
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2020-10-09 16:12:27 -0300

    finished details

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// const baseURL = 'https://github.com/abouhid/abouhid';
+
+// fetch(baseURL, {
+//     method:'GET ',
+//     headers: {
+//         'Accept': 'application/vnd.github.nebula-preview+json',
+//         'Content-type':'application/json',
+//     },
+//     body:JSON.stringify({
+//         "disease_name": disease_name, 
+//         "disease_signs": disease_signs, 
+//         "disease_symptoms": disease_symptoms, 
+//         "confirmatory_tests": confirmatory_tests 
+//     })
+// }).then( 
+//     resp => resp.json(). then( data => { 
+//         console.log(data);
+//         document.getElementById('diseases-info').reset();
+// }))
